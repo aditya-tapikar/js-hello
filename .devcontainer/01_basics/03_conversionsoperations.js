8514ff304bcffc3f4c0c51f3c9559c6fa0f593b0
aditya-tapikar <adityatapikar2003@gmail.com>
2024-06-08 18:03:01 +0000

    conversions operations

---
+++ .devcontainer/01_basics/03_conversionsoperations.js
@@ -0,0 +1,43 @@
+
+//let score="33abc"
+//console.log(typeof score)
+
+//let valueInNumber=Number(score)
+
+//console.log(typeof valueInNumber)
+
+//console.log(valueInNumber)
+
+
+//let score2=true
+
+//console.log(typeof score2)
+
+//let valueInNumber2=Number(score2)
+
+//console.log(typeof valueInNumber2)
+
+//console.log(valueInNumber2)
+
+
+
+
+
+//number converions in js
+// "33"-> 33
+//"33abc"->Nan
+//null->0
+//undefined->Nan
+//true->1  false->0
+
+let isLoggedIn="aditya"
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// boolean conversions
+
+// 1->true  0->false
+// "string"->true  ""->false
