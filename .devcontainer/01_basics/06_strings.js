3261ccd72af2a957b9b04f93cece266ad0fff67e
aditya-tapikar <adityatapikar2003@gmail.com>
2024-06-11 18:30:55 +0000

    strings in js

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,33 @@
+const name= "ADITYA"
+
+const id="0343"
+
+
+console.log(name +id)
+
+
+console.log(`hello my name is ${name} and my id is ${id}`)
+
+// start me ` ye jo symbol use hua hai isko bolte hai backticks ye tab ke upar hai 
+
+//here is another way of declaring string 
+
+const gameName= new String('adityatapikar')
+
+//some objects in String
+
+console.log(gameName[0])
+console.log(gameName.length)
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName.indexOf('t')) 
+
+
+const newString=gameName.substring(0,4)
+console.log(newString)
+
+const anotherString="    ADITYA   "
+console.log(anotherString)
+console.log(anotherString.trim())
+
