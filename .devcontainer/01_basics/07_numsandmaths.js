a6898fb71f8b142c72b2ff14f21a46e6ee494087
aditya-tapikar <adityatapikar2003@gmail.com>
2024-06-14 15:59:21 +0000

    maths and nums in js

---
+++ .devcontainer/01_basics/07_numsandmaths.js
@@ -0,0 +1,97 @@
+const score=400
+// console.log(score)
+
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+
+// console.log(balance.toString().length)
+
+const hundreds= 10000000
+
+// console.log(hundreds.toLocaleString())
+
+
+
+//++++++++Maths+++++++
+
+// console.log(Math)
+
+// console.log(Math.abs(-54))
+
+// console.log(Math.round(4.85))
+
+
+// console.log(Math.ceil(4.25))
+
+
+// console.log(Math.floor(4.75))
+
+// console.log(Math.max(5,85,54,5))
+
+
+console.log(Math.random())
+
+// THE VALUE OF MATH.RANDOM LIES BETWEEN 0 AND 1 ALWAYS
+
+console.log(Math.random()*10)
+
+console.log((Math.random()*10)+1)
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
