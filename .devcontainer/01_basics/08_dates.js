fa9ef3fadaec5ada02cddfd96365641194cfc538
aditya-tapikar <adityatapikar2003@gmail.com>
2024-06-14 21:28:30 +0000

    dates in js

---
+++ .devcontainer/01_basics/08_dates.js
@@ -0,0 +1,13 @@
+
+let myDate= new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toJSON())
+
+let myCreatedDate = new Date(2023,0,19)
+
+console.log(myCreatedDate.toLocaleDateString())
+
+let newDate= new Date()
+console.log(newDate.getMonth())
